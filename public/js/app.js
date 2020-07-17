904ab7d71b490f8a0fe31f05983f1d6a40772ca3
Justin Golden <justingolden21@gmail.com>
2020-07-16 20:42:02 -0700

    fix indentation

--- public/js/app.js
+++ public/js/app.js
@@ -3,18 +3,18 @@ const db = firebase.firestore();
 const auth = firebase.auth();
 
 function updateStatus(newName, newStatus, newEmoji, newAvailability) {
-    $('.name').html('<b>' + newName + '</bi>');
-    $('.status').html('<i>' + newStatus + '</i>');
-    $('.emoji').html(newEmoji);
-    $('.availability').html(newAvailability);
+	$('.name').html('<b>' + newName + '</bi>');
+	$('.status').html('<i>' + newStatus + '</i>');
+	$('.emoji').html(newEmoji);
+	$('.availability').html(newAvailability);
 
-    let user = auth.currentUser;
-    db.collection('users').doc(user.uid).update({
-        name: newName,
-        status: newStatus,
-        emoji: newEmoji,
-        availability: newAvailability
-    });
+	let user = auth.currentUser;
+	db.collection('users').doc(user.uid).update({
+		name: newName,
+		status: newStatus,
+		emoji: newEmoji,
+		availability: newAvailability
+	});
 
 
 }
--- public/js/signin.js
+++ public/js/signin.js
@@ -24,26 +24,26 @@ function signup() {
 	signin(true);
 }
 
-function signin(createData=false) {   
+function signin(createData=false) {
 	let provider = new firebase.auth.GoogleAuthProvider();
 
 	firebase.auth().signInWithPopup(provider).then( (result)=> {
 		let token = result.credential.accessToken;
 		user = result.user;
 
-        console.log("Successfully signed in with google account!");
+		console.log("Successfully signed in with google account!");
 
-        if(createData) {
-		    db.collection('users').doc(user.uid).set({
-		        name: user.displayName,
-		        status: 'Chilling',
-		        emoji: '😎',
-		        availability: 'available',
-		        // status: ["Chilling in my room", "Studying", "Working", "Sleeping"],
-		        // emoji: ["😎", "😊", "😃", "😄"],
-		        // availability: ["Available", "Kind of Busy", "Busy", "Away"],
-		    });
-        }
+		if(createData) {
+			db.collection('users').doc(user.uid).set({
+				name: user.displayName,
+				status: 'Chilling',
+				emoji: '😎',
+				availability: 'available',
+				// status: ["Chilling in my room", "Studying", "Working", "Sleeping"],
+				// emoji: ["😎", "😊", "😃", "😄"],
+				// availability: ["Available", "Kind of Busy", "Busy", "Away"],
+			});
+		}
 
 	}).catch( (err)=> {
 		console.log('Sign-in error', err);
